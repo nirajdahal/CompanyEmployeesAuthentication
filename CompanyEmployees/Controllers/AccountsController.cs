5a83c6f92ef10df111885198cdc63a12e44afd70
nirajdahal09@gmail.com <nirajdahal09@gmail.com>
2021-03-10 17:20:32 +0545

    added login functionality

--- CompanyEmployees/Controllers/AccountsController.cs
+++ CompanyEmployees/Controllers/AccountsController.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
+using CompanyEmployees.JwtFeatures;
 using Entities.DataTransferObjects;
 using Entities.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,11 +18,27 @@ namespace CompanyEmployees.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly IMapper _mapper;
-        public AccountsController(UserManager<User> userManager, IMapper mapper)
+        private readonly JwtHandler _jwtHandler;
+        public AccountsController(UserManager<User> userManager, IMapper mapper, JwtHandler jwtHandler)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _jwtHandler = jwtHandler;
         }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] UserForAuthenticationDto userForAuthentication)
+        {
+            var user = await _userManager.FindByNameAsync(userForAuthentication.Email);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, userForAuthentication.Password))
+                return Unauthorized(new AuthResponseDto { ErrorMessage = "Invalid Authentication" });
+            var signingCredentials = _jwtHandler.GetSigningCredentials();
+            var claims = _jwtHandler.GetClaims(user);
+            var tokenOptions = _jwtHandler.GenerateTokenOptions(signingCredentials, claims);
+            var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return Ok(new AuthResponseDto { IsAuthSuccessful = true, Token = token });
+        }
+
         [HttpPost("Registration")]
         public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
         {
--- Entities/DataTransferObjects/UserForRegistrationDto.cs
+++ Entities/DataTransferObjects/UserForRegistrationDto.cs
@@ -20,4 +20,17 @@ namespace Entities.DataTransferObjects
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
+    public class UserForAuthenticationDto
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+    public class AuthResponseDto
+    {
+        public bool IsAuthSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Token { get; set; }
+    }
 }
